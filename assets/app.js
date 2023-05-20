88aa3796f3465185ed358c752b43cd341a4a828d
Goldfinch <facu_vila@hotmail.com>
2023-05-20 16:26:04 -0300

    se agrego un color identificatorio para las respuestas correctas y las incorrectas

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,13 @@
 const DIV_BUTTONS = document.getElementById("div__buttons");
 const BTN_EASY = document.getElementById("button__easy");
 const DIV_CONTAINER = document.getElementById("div__container");
+const BTN_ANSWER_1 = document.getElementById("button__answer_1");
+const BTN_ANSWER_2 = document.getElementById("button__answer_2");
+const BTN_ANSWER_3 = document.getElementById("button__answer_3");
+const BTN_ANSWER_4 = document.getElementById("button__answer_4");
 
 let clicked = true;
+let counter = 1
 
 const SHOW_EASY_QUESTIONS = () => {
     if(clicked = true){
@@ -22,3 +27,57 @@ const SHOW_EASY_QUESTIONS = () => {
 BTN_EASY.addEventListener("click", ()=>{
     SHOW_EASY_QUESTIONS();
 })
+
+BTN_ANSWER_1.addEventListener("click", ()=>{
+    if(clicked = true && counter === 1){
+        BTN_ANSWER_1.style.backgroundColor = "green";
+        clicked = false;
+        counter++;
+    }
+    else{
+        BTN_ANSWER_1.style.backgroundColor = "#444";
+        clicked = true;
+        counter = 1;
+    }
+})
+
+BTN_ANSWER_2.addEventListener("click", ()=>{
+    if(clicked = true && counter === 1){
+        BTN_ANSWER_2.style.backgroundColor = "red";
+        clicked = false;
+        counter++;
+    }
+    else{
+        BTN_ANSWER_2.style.backgroundColor = "#444";
+        clicked = true;
+        counter = 1;
+    }
+})
+
+BTN_ANSWER_3.addEventListener("click", ()=>{
+    if(clicked = true && counter === 1){
+        BTN_ANSWER_3.style.backgroundColor = "red";
+        clicked = false;
+        counter++;
+    }
+    else{
+        BTN_ANSWER_3.style.backgroundColor = "#444";
+        clicked = true;
+        counter = 1;
+    }
+})
+
+BTN_ANSWER_4.addEventListener("click", ()=>{
+    if(clicked = true && counter === 1){
+        BTN_ANSWER_4.style.backgroundColor = "red";
+        clicked = false;
+        counter++;
+    }
+    else{
+        BTN_ANSWER_4.style.backgroundColor = "#444";
+        clicked = true;
+        counter = 1;
+    }
+})
+
+
